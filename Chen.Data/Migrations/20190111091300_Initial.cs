a1c31ebf94dca3a3a60c0c6a8ffd273217c0ddf3
jafic <158761939@qq.com>
2019-01-14 16:08:10 +0800

    取消撤销的

---
+++ Chen.Data/Migrations/20190111091300_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chen.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    CreateOperator = table.Column<string>(maxLength: 50, nullable: true),
+                    UpdateTime = table.Column<DateTime>(nullable: false),
+                    UpdateOperator = table.Column<string>(maxLength: 50, nullable: true),
+                    LoginName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Telephone = table.Column<string>(nullable: true),
+                    AnotherAdd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
