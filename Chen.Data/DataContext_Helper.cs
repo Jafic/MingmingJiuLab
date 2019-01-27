b87b2468b0ed9a06e62163588346213731ec9217
jafic <158761939@qq.com>
2019-01-27 19:58:47 +0800

    加入事务

---
+++ Chen.Data/DataContext_Helper.cs
@@ -0,0 +1,27 @@
+﻿using Chen.Tool;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chen.Data
+{
+    public partial class DataContext_Helper : DbContext
+    {
+
+        public static T Using<T>(Func<DataContext, T> getResult, bool readOnly = false)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            using (var context = new DataContext(optionsBuilder.Options))
+            {
+                var result = getResult(context);
+                if (!readOnly && result is ActionResult)
+                {
+                    //context.ActionByResult(result as ActionResult);
+                }
+                return result;
+            }
+        }
+
+    }
+}
---
+++ Chen.Tool/ActionResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chen.Tool
+{
+    public class ActionResult
+    {
+        /// <summary>
+        /// 操作成功
+        /// </summary>
+        public static ActionResult Success
+        {
+            get
+            {
+                return new ActionResult { IsSuccess = true, Message = string.Empty };
+            }
+        }
+
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>
+        /// 操作消息
+        /// </summary>
+        public string Message { get; set; }
+    }
+
+    /// <summary>
+    /// 含数据的操作结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ActionResult<T> : ActionResult
+    {
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public T Data { get; set; }
+    }
+}
